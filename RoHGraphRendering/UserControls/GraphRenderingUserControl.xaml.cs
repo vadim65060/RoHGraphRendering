000872c4f1dc42a4a00e6292670f226c846e1f6c
vadim65060 <vadim65060@users.noreply.github.com>
2021-12-10 19:19:36 +0300

    Добавьте файлы проекта.

---
+++ RoHGraphRendering/UserControls/GraphRenderingUserControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RoHGraphRendering.ViewModels;
+
+namespace RoHGraphRendering.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для GraphRenderingUserControl.xaml
+    /// </summary>
+    public partial class GraphRenderingUserControl : UserControl
+    {
+        private readonly GraphRenderingViewModel _viewModel;
+
+        public GraphRenderingUserControl()
+        {
+            InitializeComponent();
+            _viewModel = new GraphRenderingViewModel();
+            DataContext = _viewModel;
+            _viewModel.Init();
+        }
+
+        public void SetConstants(double brightness, double range, double attenuation0, double attenuation1,
+            double attenuation2)
+        {
+            _viewModel.SetConstants(brightness, range, attenuation0, attenuation1, attenuation2);
+        }
+    }
+}
---
+++ RoHGraphRendering/ViewModels/GraphRenderingViewModel.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OxyPlot;
+using RoHGraphRendering.Сlasses;
+
+namespace RoHGraphRendering.ViewModels
+{
+    class GraphRenderingViewModel : INotifyPropertyChanged
+    {
+        public PlotModel Model { get; } = new PlotModel();
+        private Graph graph = new();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Init()
+        {
+            ModelUpdate();
+        }
+
+        private void ModelUpdate()
+        {
+            Model.Series.Clear();
+            Model.Series.Add(graph.PointsCalculate());
+            Model.InvalidatePlot(true);
+        }
+
+        public void SetConstants(double brightness, double range, double attenuation0, double attenuation1,
+            double attenuation2)
+        {
+            graph.Brightness = brightness;
+            graph.Range = range;
+            graph.Attenuation0 = attenuation0;
+            graph.Attenuation1 = attenuation1;
+            graph.Attenuation2 = attenuation2;
+            ModelUpdate();
+        }
+
+        public double Brightness
+        {
+            get => graph.Brightness;
+            set
+            {
+                graph.Brightness = value;
+                ModelUpdate();
+            }
+        }
+
+        public double Range
+        {
+            get => graph.Range;
+            set
+            {
+                graph.Range = value;
+                ModelUpdate();
+            }
+        }
+
+        public double Attenuation0
+        {
+            get => graph.Attenuation0;
+            set
+            {
+                graph.Attenuation0 = value;
+                ModelUpdate();
+            }
+        }
+
+        public double Attenuation1
+        {
+            get => graph.Attenuation1;
+            set
+            {
+                graph.Attenuation1 = value;
+                ModelUpdate();
+            }
+        }
+
+        public double Attenuation2
+        {
+            get => graph.Attenuation2;
+            set
+            {
+                graph.Attenuation2 = value;
+                ModelUpdate();
+            }
+        }
+    }
+}
